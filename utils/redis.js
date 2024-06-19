e56c58d6d9b856da0391d0c3eda3899014742ef2
eungobs <eungobs@gmail.com>
2024-06-19 19:29:15 +0000

    utils

--- utils/redis.js
+++ utils/redis.js
@@ -1,38 +1,62 @@
 import redis from 'redis';
 import { promisify } from 'util';
 
+/**
+ * Class for performing operations with Redis service
+ */
 class RedisClient {
   constructor() {
-    const client = redis.createClient();
-    this.value = true;
-    client.on('error', (err) => {
-      console.log(err);
-      this.value = false;
+    this.client = redis.createClient();
+    this.getAsync = promisify(this.client.get).bind(this.client);
+
+    this.client.on('error', (error) => {
+      console.log(`Redis client not connected to the server: ${error.message}`);
     });
-    client.on('connect', () => {
-      this.value = true;
+
+    this.client.on('connect', () => {
+      // console.log('Redis client connected to the server');
     });
-    this.client = client;
   }
 
+  /**
+   * Checks if connection to Redis is Alive
+   * @return {boolean} true if connection alive or false if not
+   */
   isAlive() {
-    return this.value;
+    return this.client.connected;
   }
 
+  /**
+   * gets value corresponding to key in redis
+   * @key {string} key to search for in redis
+   * @return {string}  value of key
+   */
   async get(key) {
-    return promisify(this.client.GET).bind(this.client)(key);
+    const value = await this.getAsync(key);
+    return value;
   }
 
-  async set(key, value, time) {
-    this.client.set(key, value, () => {
-      this.client.expire(key, time);
-    });
+  /**
+   * Creates a new key in redis with a specific TTL
+   * @key {string} key to be saved in redis
+   * @value {string} value to be asigned to key
+   * @duration {number} TTL of key
+   * @return {undefined}  No return
+   */
+  async set(key, value, duration) {
+    this.client.setex(key, duration, value);
   }
 
+  /**
+   * Deletes key in redis service
+   * @key {string} key to be deleted
+   * @return {undefined}  No return
+   */
   async del(key) {
     this.client.del(key);
   }
 }
 
 const redisClient = new RedisClient();
+
 export default redisClient;
